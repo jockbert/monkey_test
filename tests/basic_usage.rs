17bc5fd49d157b1d37440167a1fd93eaa2871d76
Joakim Akerblom <per.joakim.eriksson@gmail.com>
2023-09-29 11:45:44 +0200

    Style: Rename basic_usage tests for readability

--- tests/basic_usage.rs
+++ tests/basic_usage.rs
@@ -1,14 +1,14 @@
 use monkey_test::*;
 
 #[test]
-fn test_add_up_to_overflow() {
+fn add_up_to_overflow() {
     monkey_test()
         .with_generator(gen::u8::any())
         .assert_true(|x: u8| x == 255 || x + 1 > x)
 }
 
 #[test]
-fn test_can_fail_with_details_when_using_check() {
+fn can_fail_with_details_when_using_check() {
     let actual_result: MonkeyResult<u8> = monkey_test()
         .with_seed(123456)
         .with_generator(gen::fixed::sequence(&[1, 2, 3, 10, 20, 30]))
@@ -30,7 +30,7 @@ fn test_can_fail_with_details_when_using_check() {
 
 #[test]
 #[should_panic(expected = "Monkey test property failed!\nCounterexample: 15")]
-fn test_can_fail_with_panic_when_using_assert() {
+fn can_fail_with_panic_when_using_assert() {
     monkey_test()
         .with_seed(123456)
         .with_generator(gen::fixed::sequence(&[1, 2, 3, 10, 20, 30]))
@@ -39,7 +39,7 @@ fn test_can_fail_with_panic_when_using_assert() {
 }
 
 #[test]
-fn monkey_test_setting_all_settings_available() {
+fn use_all_settings_available() {
     monkey_test()
         .with_example_count(1_000)
         .with_seed(1234567890)
