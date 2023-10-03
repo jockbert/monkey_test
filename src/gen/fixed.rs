50015fc2cae6b5980c6ff2f4be3980d3e6864de0
Joakim Akerblom <per.joakim.eriksson@gmail.com>
2023-10-03 21:41:51 +0200

    Refactor!: Rename SliceGen to SequenceGen

--- src/gen/fixed.rs
+++ src/gen/fixed.rs
@@ -1,7 +1,7 @@
 //! Generators mainly used for internal testing, where you want a
 //! deterministicly generated values.
 
-use crate::shrink::NoShrink;
+use crate::{shrink::NoShrink, Gen};
 
 /// Generates a fixed sequence of examples.
 ///
@@ -9,39 +9,46 @@ use crate::shrink::NoShrink;
 /// use crate::monkey_test::Gen;
 ///
 /// let gen = monkey_test::gen::fixed::sequence::<u64>(&[1,20,300]);
-/// let mut ex = gen.examples(1337);
-/// assert_eq!(Some(1), ex.next());
-/// assert_eq!(Some(20), ex.next());
-/// assert_eq!(Some(300), ex.next());
-/// assert_eq!(None, ex.next());
-/// assert_eq!(None, ex.next());
-/// assert_eq!(None, ex.next());
+///
+/// let mut ex1 = gen.examples(1337);
+/// assert_eq!(Some(1), ex1.next());
+/// assert_eq!(Some(20), ex1.next());
+/// assert_eq!(Some(300), ex1.next());
+/// assert_eq!(None, ex1.next());
+/// assert_eq!(None, ex1.next());
+/// assert_eq!(None, ex1.next());
+///
+/// let mut ex2 = gen.examples(42);
+/// assert_eq!(Some(1), ex2.next());
+/// assert_eq!(Some(20), ex2.next());
+/// assert_eq!(Some(300), ex2.next());
+/// assert_eq!(None, ex2.next());
 /// ```
-pub fn sequence<E>(examples: &[E]) -> SliceGen<E>
+pub fn sequence<E>(examples: &[E]) -> SequenceGen<E>
 where
     E: Clone + std::fmt::Debug,
 {
-    SliceGen::new(examples)
+    SequenceGen::new(examples)
 }
 
 /// Generator from a given set of examples to return.
 #[derive(Clone)]
-pub struct SliceGen<E> {
+pub struct SequenceGen<E> {
     data: Vec<E>,
 }
 
-impl<E> SliceGen<E>
+impl<E> SequenceGen<E>
 where
     E: Clone,
 {
     fn new(data: &[E]) -> Self {
-        SliceGen {
+        SequenceGen {
             data: Vec::from(data),
         }
     }
 }
 
-impl<E: Clone + 'static> crate::Gen<E, NoShrink<E>> for SliceGen<E> {
+impl<E: Clone + 'static> Gen<E, NoShrink<E>> for SequenceGen<E> {
     fn examples(&self, _seed: u64) -> crate::SomeIter<E> {
         let x = self.data.clone();
         Box::new(x.into_iter())
