2ab8bed5bc6a3c159eae5f1c7dd1061a262b7fdb
Joakim Akerblom <per.joakim.eriksson@gmail.com>
2023-11-11 23:25:35 +0100

    Test: Add assert_generator_is_empty

--- src/testing.rs
+++ src/testing.rs
@@ -1,2 +1,12 @@
 pub mod distribution;
 pub mod numbers;
+
+use crate::BoxGen;
+
+/// Makes sure generator is empty
+pub fn assert_generator_is_empty<E>(gen_to_check: BoxGen<E>)
+where
+    E: Clone + 'static,
+{
+    assert_eq!(gen_to_check.examples(1234).count(), 0);
+}
