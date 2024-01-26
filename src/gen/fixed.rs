a1d64d1d8183f316f80c487ac353c65847059cb5
Joakim Akerblom <per.joakim.eriksson@gmail.com>
2024-01-26 13:54:15 +0100

    Feat: Add generator fixed::in_loop

--- src/gen/fixed.rs
+++ src/gen/fixed.rs
@@ -1,14 +1,12 @@
 //! Generators mainly used for internal testing, where you want a
 //! deterministicly generated values.
 
-use crate::{BoxGen, BoxShrink, Gen};
+use crate::{BoxGen, BoxIter, BoxShrink, Gen};
 
-/// Generates a fixed sequence of examples.
+/// Generates a fixed sequence of examples and then ends, having no more values.
 ///
 /// ```
-/// use crate::monkey_test::Gen;
-///
-/// let gen = monkey_test::gen::fixed::sequence::<u64>(&[1,20,300]);
+/// let gen = monkey_test::gen::fixed::sequence(&[1, 20, 300]);
 ///
 /// let mut ex1 = gen.examples(1337);
 /// assert_eq!(Some(1), ex1.next());
@@ -28,7 +26,9 @@ pub fn sequence<E>(examples: &[E]) -> BoxGen<E>
 where
     E: Clone + std::fmt::Debug + 'static,
 {
-    Box::new(SequenceGen::new(examples))
+    Box::new(SequenceGen {
+        data: examples.to_vec(),
+    })
 }
 
 /// Generator from a given set of examples to return.
@@ -37,24 +37,70 @@ struct SequenceGen<E> {
     data: Vec<E>,
 }
 
-impl<E> SequenceGen<E>
-where
-    E: Clone,
-{
-    fn new(data: &[E]) -> Self {
-        SequenceGen {
-            data: Vec::from(data),
-        }
-    }
-}
-
 impl<E: Clone + 'static> Gen<E> for SequenceGen<E> {
-    fn examples(&self, _seed: u64) -> crate::BoxIter<E> {
+    fn examples(&self, _seed: u64) -> BoxIter<E> {
         let x = self.data.clone();
         Box::new(x.into_iter())
     }
 
     fn shrinker(&self) -> BoxShrink<E> {
         crate::shrink::none()
+    }
+}
+
+/// Generates a fixed loop of examples. This generator is convenient when you,
+/// for instance, want to know the exact distribution of the generator. The
+/// distribition is not always exact when dealing with randomness.
+///
+/// ```
+/// let looper = monkey_test::gen::fixed::in_loop(&[1, 20, 300]);
+///
+/// let examples = looper.examples(42).take(10).collect::<Vec<_>>();
+/// assert_eq!(examples, vec![1, 20, 300, 1, 20, 300, 1, 20, 300, 1]);
+///
+/// // Returns the same examples when using other seed
+/// let other_seed = looper.examples(1337).take(10).collect::<Vec<_>>();
+/// assert_eq!(examples, other_seed);
+/// ```
+pub fn in_loop<E>(examples: &[E]) -> BoxGen<E>
+where
+    E: Clone + 'static,
+{
+    Box::new(LoopGen {
+        data: examples.to_vec(),
+    })
+}
+
+/// Generator from a given set of examples to return in loop.
+#[derive(Clone)]
+struct LoopGen<E> {
+    data: Vec<E>,
+}
+
+impl<E: Clone + 'static> Gen<E> for LoopGen<E> {
+    fn examples(&self, _seed: u64) -> BoxIter<E> {
+        let x = self.data.clone();
+        Box::new(LoopIter { data: x, index: 0 })
+    }
+
+    fn shrinker(&self) -> BoxShrink<E> {
+        crate::shrink::none()
+    }
+}
+
+/// Generator from a given set of examples to return in loop.
+#[derive(Clone)]
+struct LoopIter<E> {
+    data: Vec<E>,
+    index: usize,
+}
+
+impl<E: Clone + 'static> Iterator for LoopIter<E> {
+    type Item = E;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        let index_to_use = self.index;
+        self.index = (self.index + 1) % self.data.len();
+        self.data.get(index_to_use).cloned()
     }
 }
