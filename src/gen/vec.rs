1c1b0a58b6b28a18a475e4e397bd87fe5d1ebaff
Joakim Akerblom <per.joakim.eriksson@gmail.com>
2024-02-16 22:45:54 +0100

    Refactor: Improve and clarify vector generator
    - Use sized generator for vector size. Length can now be
      larger than `u8::MAX`.
    - Simplify implementation by using zip and map.

--- src/gen/vec.rs
+++ src/gen/vec.rs
@@ -4,50 +4,34 @@ use crate::BoxGen;
 use crate::BoxIter;
 use crate::BoxShrink;
 use crate::Gen;
-use rand::{Rng, SeedableRng};
-use rand_chacha::ChaCha8Rng;
 
 /// Any vector filled with values from given element generator
 pub fn any<E: Clone + 'static>(element_gen: BoxGen<E>) -> BoxGen<Vec<E>> {
-    Box::new(RandVecGen::<E> { element_gen })
+    Box::new(VecGen::<E> { element_gen })
 }
 
-/// Generator for random vectors.
 #[derive(Clone)]
-struct RandVecGen<E> {
+struct VecGen<E> {
     element_gen: BoxGen<E>,
 }
 
-impl<E: Clone + 'static> Gen<Vec<E>> for RandVecGen<E> {
+impl<E> Gen<Vec<E>> for VecGen<E>
+where
+    E: Clone + 'static,
+{
     fn examples(&self, seed: u64) -> BoxIter<Vec<E>> {
-        Box::new(RandVecIter::<E> {
-            rng: rand_chacha::ChaCha8Rng::seed_from_u64(seed),
-            element_it: self.element_gen.examples(seed),
-        })
+        let sizes = crate::gen::sized::default().examples(seed);
+        let seeds = crate::gen::u64::completely_random(..).examples(seed);
+        let element_gen = self.element_gen.clone();
+
+        let vectors = sizes.zip(seeds).map(move |(size, seed)| {
+            element_gen.examples(seed).take(size).collect::<Vec<_>>()
+        });
+
+        Box::new(vectors)
     }
 
     fn shrinker(&self) -> BoxShrink<Vec<E>> {
         crate::shrink::vec::default()
-    }
-}
-
-/// Iterator of random vectors.
-struct RandVecIter<E> {
-    rng: ChaCha8Rng,
-    element_it: crate::BoxIter<E>,
-}
-
-impl<E> Iterator for RandVecIter<E> {
-    type Item = Vec<E>;
-
-    fn next(&mut self) -> Option<Self::Item> {
-        let f = self.rng.gen::<u16>();
-        let length = f as u8;
-
-        let mut res: Vec<E> = Vec::new();
-        for _ in 0..length {
-            res.push(self.element_it.next().expect(""))
-        }
-        Some(res)
     }
 }
